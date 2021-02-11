3ae0d76988ba230ad0720d9c35e5541207170d6b
katyou <ienaga@ijgn.jp>
2021-02-11 19:22:05 +0900

    style練習

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import '../styles/global.css'
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
